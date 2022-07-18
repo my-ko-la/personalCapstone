4fdc7a190e34bf4327698e4949a296a4ab33eeca
vvarhand <63235046+vvarhand@users.noreply.github.com>
2022-07-18 16:42:45 +0200

    got everything working

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,9 +3,11 @@ import storeItems from '../data/storeItems.json';
 export default function Store() {
     return (
         <>
-            {storeItems.map(item => {
-                <p className="text-blue-300">JSON.stringify(item)</p>;
-            })}
+        <h1>Store</h1>
+
+  {storeItems.map(item => (
+   <p className="text-blue-300">{JSON.stringify(item)}</p>
+            ))}
         </>
     )
 } 
