ff4043ba603b60b56e50b3ab13f3bff7d0c927ca
vvarhand <63235046+vvarhand@users.noreply.github.com>
2022-07-25 17:22:33 +0200

    Add top links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import TopBar from "./components/TopBar";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
     <>
-      <TopBar />
+      <Home />
     </>
   );
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import TopBar from "../components/TopBar"
+
+
+const Home = () => 
+{
+    return (
+        <>
+            <TopBar />
+            <Navbar />
+            <hr></hr>
+        </>
+    )
+}
+
+export default Home;
