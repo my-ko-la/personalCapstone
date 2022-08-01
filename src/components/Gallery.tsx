e8aaea10a315aec048a5564ed6bdaa0a42b6cbe7
vvarhand <brechko.mv@gmail.com>
2022-08-01 20:00:50 +0200

    Carousel and PreFooter visuals

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,29 @@
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';   
+import ShopItem from './ShopItem';
+import data from '../data/shop.json';   
+
+const Gallery = () => {
+
+    const handleDragStart = ( e: any ) => e.preventDefault();
+
+    const shopItems = data.map((item) =>
+        <ShopItem {...item} />
+        )
+
+    const responsive=
+                {
+                    0:{items:1,},
+                    396:{items:2,},
+                    768:{items:4,},
+                    1440: {items:5},
+                }
+    return (    
+        <AliceCarousel mouseTracking 
+        disableDotsControls={true}
+        infinite={true}
+        responsive={responsive} items={shopItems} />
+    )
+}
+
+export default Gallery;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,28 +3,29 @@ import TopBar from "../components/TopBar";
 import msiWallpaper from "/images/msi_desktop.png";
 import ShopItem from "../components/ShopItem";
 import data from "../data/shop.json";
+import Gallery from "../components/Gallery";
+import PreFooter from "../components/PreFooter";
+
 
 const Home = () => 
 {
-    const shopItems = data.map((item) =>
-    <ShopItem {...item} />
-    )
     
     return (
         <>
             <TopBar />
             <Navbar />
             <hr></hr>
-            <img className="sm:flex justify-center hidden md:inline-flex  lg:min-w-[1120px] md:px-48" src={msiWallpaper}></img>
+            <img className="sm:flex justify-center hidden md:inline-flex md:px-48" src={msiWallpaper}></img>
             <div className="pt-10"></div>
             <div className="flex flex-row px-24 md:px-48 justify-between">
                 <span className="font-bold">New Products</span>
-                <a className="text-blue-300 underline text-xs">See All Products</a>
+                <a className="text-blue-300 underline text-xs">See All New Products</a>
             </div>
             <div className="pt-6"></div>
-            <div className="px-48 flex flex-row gap-6">
-                {shopItems} 
-            </div> 
+           <div className="md:px-48">
+                <Gallery /> 
+            </div>
+            <PreFooter />
         </>
     )
 }
