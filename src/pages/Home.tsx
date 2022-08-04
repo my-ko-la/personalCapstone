4ac9cf7348749b0fa2df613b78ef14a9d30610d2
vvarhand <brechko.mv@gmail.com>
2022-08-04 15:19:00 +0200

    Write Footer code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,17 +16,19 @@ const Home = () =>
             <TopBar />
             <Navbar />
             <hr></hr>
-            <img className="sm:flex justify-center hidden md:inline-flex md:px-48" src={msiWallpaper}></img>
-            <div className="pt-10"></div>
-            <div className="flex flex-row px-24 md:px-48 justify-between">
-                <span className="font-bold">New Products</span>
-                <a className="text-blue-300 underline text-xs">See All New Products</a>
-            </div>
-            <div className="pt-6"></div>
-           <div className="md:px-48">
-                <Gallery /> 
-            </div>
-            <PreFooter />
+            <main className="mb-auto">
+                <img className="sm:flex justify-center hidden md:inline-flex md:px-48" src={msiWallpaper}></img>
+                <div className="pt-10"></div>
+                <div className="flex flex-row px-24 md:px-48 justify-between">
+                    <span className="font-bold">New Products</span>
+                    <a className="text-blue-300 underline text-xs">See All New Products</a>
+                </div>
+                <div className="pt-6"></div>
+                <div className="md:px-48">
+                    <Gallery /> 
+                </div>
+                <PreFooter />
+            </main>
             <Footer />
         </>
     )
