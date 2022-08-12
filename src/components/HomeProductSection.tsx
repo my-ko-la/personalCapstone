92a8e21d69201b6fdc6c80e07082ccec716c04c7
vvarhand <brechko.mv@gmail.com>
2022-08-12 14:38:18 +0200

    Setup Spring animations

--- src/components/HomeProductSection.tsx
+++ src/components/HomeProductSection.tsx
@@ -1,9 +1,9 @@
 import type {ShopItemProps} from './ShopItem';
-import ShopItem from "./ShopItem";
 import data from '../data/shop.json';
 import { useState } from "react";
 import renderProductByCategory from '../utils/renderProductByCategory';
 
+import {animated, useSprings} from '@react-spring/web';
 
 
 interface HomeProductSectionProps {
@@ -16,9 +16,48 @@ interface HomeProductSectionProps {
     shopItems?: ShopItemProps
 }
 
-const HomeProductSection: React.FunctionComponent<HomeProductSectionProps> = (props) => {
-    
-    // find a better way yeah?
+const HomeProductSection: React.FunctionComponent<HomeProductSectionProps> = (props) => { 
+
+    // animation instructions
+    const animInst = 
+    [
+        {
+            id:1,
+            from: {opacity: 0, marginLeft: -10},
+            to: {opacity: 1, marginLeft: 0},
+            config: {duration: 500},
+            delay: 200,
+        },
+        {
+            id:2,
+            from: {opacity: 0, marginLeft: -20},
+            to: {opacity: 1, marginLeft: 0},
+            config: {duration: 400},
+            delay: 300,
+        },
+        {
+            id:3,
+            from: {opacity: 0, marginLeft: -30},
+            to: {opacity: 1, marginLeft: 0},
+            config: {duration: 300},
+            delay: 400,
+        }, 
+        {
+            id:4,
+            from: {opacity: 0, marginLeft: -40},
+            to: {opacity: 1, marginLeft: 0},
+            config: {duration: 200},
+            delay: 500,
+        },
+    ];
+
+    const springs = useSprings(
+        animInst.length,
+        animInst.map(({id, ...param}) => param)
+    )
+
+
+
     const [categoryShown, setCategoryShown] = 
     useState(props.categoryList ? props.categoryList[0] : "");
     
