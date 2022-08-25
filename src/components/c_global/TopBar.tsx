7730a8f46d6666665de08571db03050b782bae03
vvarhand <brechko.mv@gmail.com>
2022-08-25 18:43:49 +0200

    TopBar Contact Us now links to contact page

--- src/components/c_global/TopBar.tsx
+++ src/components/c_global/TopBar.tsx
@@ -1,21 +1,26 @@
 import OpeningHours from "./OpeningHours";
 import NumbersAndSocials from "./NumberAndSocials";
+import { Link } from "react-router-dom";
 
-const TopBar = () =>
-{
-    return (
-        <div className="h-11 bg-black flex flex-row justify-around text-white
-        items-center md:px-28">
-            <OpeningHours />
-           <div className="flex flex-row gap-1 text-md md:text-xs transition-all ease-linear duration-200">
-               <h4 className="text-gray-400">Visit our showroom at 1234 Street, Address City</h4>
-               <a className="underline" href="#">Contact Us</a>
-            </div>
-           
-           <NumbersAndSocials /> 
-        
-        </div>
-    )
-}
+const TopBar = () => {
+  return (
+    <div
+      className="h-11 bg-black flex flex-row justify-around text-white
+        items-center md:px-28"
+    >
+      <OpeningHours />
+      <div className="flex flex-row gap-1 text-md md:text-xs transition-all ease-linear duration-200">
+        <h4 className="text-gray-400">
+          Visit our showroom at 1234 Street, Address City
+        </h4>
+        <Link to="/contact" className="underline">
+          Contact Us
+        </Link>
+      </div>
+
+      <NumbersAndSocials />
+    </div>
+  );
+};
 
 export default TopBar;
