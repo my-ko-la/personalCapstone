0190c6851305c4dcd3ca08642ee58b68af72100f
vvarhand <brechko.mv@gmail.com>
2022-09-07 00:20:44 +0200

    Styling login input fields

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,22 +1,29 @@
 const LoginForm = () => {
   return (
-    <div className="font-poppins flex flex-col max-w-xl bg-slate-50 shadow-md items-center">
-      <h1 className="pt-4">Login</h1>
+    <div className="font-poppins flex flex-col max-w-xl items-center">
+      <p className="pt-4 text-2xl">Login</p>
       <form
         className="flex flex-col gap-5 items-center rounded-xl justify-center py-10 px-5"
         action=""
       >
         <label className="flex flex-col gap-2" htmlFor="">
           Email:
-          <input className="rounded" type="text" />
+          <input
+            className="block mt-0 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black"
+            type="text"
+          />
         </label>
 
         <label className="flex flex-col gap-2" htmlFor="">
           Password:
-          <input type="password" />
+          <input
+            className="block mt-0 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black"
+            type="password"
+          />
         </label>
       </form>
       <hr />
+      <p>Sign up?</p>
     </div>
   );
 };
